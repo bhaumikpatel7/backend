cb6e20ab02dcd365d99fca464a0aec70add53450
bhaumikpatel7 <72443943+bhaumikpatel7@users.noreply.github.com>
2024-01-30 13:52:11 -0500

    final changes on nodemailer

--- 6.video-photoUploadApp/models/File.js
+++ 6.video-photoUploadApp/models/File.js
@@ -1,20 +1,51 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const fileSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    imageUrl:{
-        type:String,
-    },
-    tags:{
-        type:String,
-    },
-    email:{
-        type:String,
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+  },
+  tags: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+});
 
-const File = mongoose.model("File",fileSchema);
+fileSchema.post("save", async function (doc) {
+  try {
+    console.log("Doc", doc);
+
+    //transpoter
+    let transporter = nodemailer.createTransport({
+        host:process.env.MAIL_HOST,
+        auth:{
+            user:process.env.MAIL_USER,
+            pass:process.env.MAIL_PASS,
+        },
+    });
+
+    //send  mail
+
+    const info = transporter.sendMail({
+        from: 'From Priyansh',
+        to: doc.email,
+        subject: "New File Uploaded to Cloudinary",
+        html: `<h2>File Uploaded</h2> <br> view now - <a href="${doc.fileUrl}">CLick Here</a>`
+    })
+    console.log(info);
+
+
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const File = mongoose.model("File", fileSchema);
 module.exports = File;
